d488f12eef6ff63337350ed7aa5480504993c05b
jitendragoswami202 <jitendragoswami202@gmail.com>
2025-03-21 14:12:30 +0530

    Delete netlify/functions/webhook.js

--- netlify/functions/webhook.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-    console.log("Webhook received:", event.body);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Webhook received" }),
-    };
-};
