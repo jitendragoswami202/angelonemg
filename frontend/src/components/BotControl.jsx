388fc230f8af9cf00d41606f51acc224e2f069e7
jitendragoswami202 <jitendragoswami202@gmail.com>
2025-05-03 11:07:18 +0530

    Update BotControl.jsx

---
+++ frontend/src/components/BotControl.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BotControl = () => {
+  return (
+    <div>
+      <button>Start Bot</button>
+      <button>Stop Bot</button>
+    </div>
+  );
+};
+
+export default BotControl;
