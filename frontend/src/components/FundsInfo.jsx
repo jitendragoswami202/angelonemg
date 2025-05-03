66cb6945a69b3754a31fcff5eea2b0999c85e5d7
jitendragoswami202 <jitendragoswami202@gmail.com>
2025-05-03 11:07:43 +0530

    Update FundsInfo.jsx

---
+++ frontend/src/components/FundsInfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FundsInfo = () => {
+  return (
+    <div>
+      <h2>Funds Info</h2>
+      <p>Available Funds: $10,000</p>
+    </div>
+  );
+};
+
+export default FundsInfo;
