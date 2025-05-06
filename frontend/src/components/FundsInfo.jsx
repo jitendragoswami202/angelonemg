3c1201fcd9648a58d135a7793b12f6f5f724cd2d
jitendragoswami202 <jitendragoswami202@gmail.com>
2025-05-06 08:05:47 +0530

    Update FundsInfo.jsx

--- frontend/src/components/FundsInfo.jsx
+++ frontend/src/components/FundsInfo.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const FundsInfo = () => {
+  const [funds, setFunds] = useState(null);
+
+  useEffect(() => {
+    axios.get('/api/funds').then((res) => setFunds(res.data));
+  }, []);
+
   return (
     <div>
-      <h2>Funds Info</h2>
-      <p>Available Funds: $10,000</p>
+      <h4>Funds Info</h4>
+      {funds ? (
+        <ul>
+          {Object.entries(funds).map(([currency, balance]) => (
+            <li key={currency}>{currency}: {balance}</li>
+          ))}
+        </ul>
+      ) : 'Loading...'}
     </div>
   );
 };
